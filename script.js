3f229954892126adb15f60fb65338ab745647471
VenkataReddyPadala <84061954+VenkataReddyPadala@users.noreply.github.com>
2025-11-07 16:06:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const line = document.querySelector("#line");
+let angle = 0;
+function rotate() {
+  angle += 2;
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+  line.style.transformOrigin = "center";
+}
+setInterval(rotate, 20);
